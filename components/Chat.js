15fb079b10f677b8e0930e4d1ea9f6b304ef40da
javapak24 <ajava2424@gmail.com>
2024-08-17 19:40:37 -0400

    5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,37 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform} from 'react-native';
 
-import { GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = () => {
+  
+}
+
+const ChatScreen = ({ route, navigation }) => {
+  const { name , background } = route.params;
   const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    navigation.setOptions({ title: name, color: background });
+  }, []);
+  
   
   useEffect(() => {
     setMessages([
       {
         _id: 1,
-        text: "Hello developer",
+        text: 'Hello developer',
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
         },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
       },
     ]);
   }, []);
@@ -25,30 +40,41 @@ const Chat = () => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
   }
 
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
+
   return (
+    <View style={[styles.mcontainer, {backgroundColor: background}]}>
     <GiftedChat
       messages={messages}
+      renderBubble={renderBubble}
       onSend={messages => onSend(messages)}
       user={{
         _id: 1
       }}
     />
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+    {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
+    </View>
   )
-}
-
-const ChatScreen = ({ route, navigation }) => {
-  const { name , background } = route.params;
-
-  useEffect(() => {
-    navigation.setOptions({ title: name, color: background });
-  }, []);
-
- return (
-   <View style={[styles.container,
-   {backgroundColor: background}]}>
-     <Text>Hello {name}!</Text>
-   </View>
- );
+//  return (
+//    <View style={[styles.container,
+//    {backgroundColor: background}]}>
+//      <Text>Hello {name}!</Text>
+//    </View>
+//  );
 }
 
 const styles = StyleSheet.create({
@@ -56,6 +82,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  mcontainer: {
+    flex: 1
   }
 });
 
