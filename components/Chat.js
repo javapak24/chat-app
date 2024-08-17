7871c554c400898449393b0ba76b09b75b843970
javapak24 <ajava2424@gmail.com>
2024-08-17 16:32:44 -0400

    ..

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+
+import { GiftedChat } from "react-native-gifted-chat";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
+  )
+}
 
 const ChatScreen = ({ route, navigation }) => {
   const { name , background } = route.params;
